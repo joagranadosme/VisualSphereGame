00ce09cbe6b3a393c40fb461a371e97855d1d4df
joagranadosme <joagranadosme@unal.edu.co>
2018-05-29 17:11:33 -0500

    Adding processing JAR and working

---
+++ app/src/main/java/com/example/jonathan/processingtest/Sketch.java
@@ -0,0 +1,21 @@
+package com.example.jonathan.processingtest;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+        if(mousePressed) {
+            ellipse(mouseX, mouseY, 50, 50);
+        }
+    }
+
+}
